8a264350b644116a02e83122aed0859c023c5265
Dani <discariote@gmail.com>
2018-04-10 00:34:57 +0200

    Subido prototipo de proyecto de Unity

---
+++ PruebaJuego/Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimientoCamara : MonoBehaviour {
+
+    GameObject jugador;
+	// Use this for initialization
+	void Start () {
+        //Asociamos ,nada mas crear el objeto(comenzar la escena), un gameobject al que tenga la etiqueta de "player"
+        jugador = GameObject.FindGameObjectWithTag("Player");
+       
+	}
+	
+	// Sobreescribimos el evento LateUpdate para que la ejecución del código sea en el ultimo frame de movimiento.
+	void LateUpdate () {
+        //El transform de la camara se acopla con la posición del transform del jugador, haciendo asi que siempre este fijado en el.
+        transform.position = jugador.transform.position;
+	}
+}
